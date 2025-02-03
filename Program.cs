dd1afbedd56578bc8a314d05205eaa31ee52f450
Евгений Кучма <desferto87@gmail.com>
2025-02-03 16:28:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HomeWorkArrayWorkRowsColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstColumnMultiplication  = 1;
+            int secondLineAmount = 0;
+
+            int[,] array = new int[3, 5] {
+                { 3, 5, 6, 7, 8 },
+                { 3, 3, 3, 3, 3 },
+                { 3, 5, 6, 7, 8 }
+            };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                    if (j == 0)
+                    {
+                       firstColumnMultiplication *= array[i, j];
+                    }
+                    if (i == 1)
+                    {
+                        secondLineAmount += array[i, j];
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine(firstColumnMultiplication);
+            Console.WriteLine(secondLineAmount);
+        }
+    }
+}
