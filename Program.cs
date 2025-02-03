a683ddf4543adbb8b800f62ac7d5c644503c8fdb
Евгений Кучма <desferto87@gmail.com>
2025-02-03 19:28:39 +0500

    lv

--- Program.cs
+++ Program.cs
@@ -6,50 +6,38 @@ namespace HomeWorkArrayWorkRowsColumns
     {
         static void Main(string[] args)
         {
-            int columnNumber = 0;
-            int rowNumber = 1;
+            int columnIndex = 0;
+            int rowIndex = 1;
             int columnMultiplication = 1;
-            int lineAmount = 0;
+            int lineSum = 0;
 
-            int[,] numberesMatrixArray = new int[3, 5] {
+            int[,] numberes = new int[3, 5] {
                 { 3, 5, 6, 7, 8 },
                 { 3, 3, 3, 3, 3 },
                 { 3, 5, 6, 7, 8 }
             };
 
-            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
+            for (int i = 0; i < numberes.GetLength(0); i++)
             {
-                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
+                for (int j = 0; j < numberes.GetLength(1); j++)
                 {
-                    Console.Write(numberesMatrixArray[i, j] + "\t");
+                    Console.Write(numberes[i, j] + "\t");
                 }
                 Console.WriteLine("\n");
             }
 
-            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
+            for (int i = 0; i < numberes.GetLength(0); i++)
             {
-                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
-                {
-                    if (j == columnNumber)
-                    {
-                        columnMultiplication *= numberesMatrixArray[i, j];
-                    }
-                }
+                columnMultiplication *= numberes[i, columnIndex];
             }
 
-            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
+            for (int i = 0; i < numberes.GetLength(1); i++)
             {
-                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
-                {
-                    if (i == rowNumber)
-                    {
-                        lineAmount += numberesMatrixArray[i, j];
-                    }
-                }
+                lineSum += numberes[rowIndex, i];
             }
 
             Console.WriteLine(columnMultiplication);
-            Console.WriteLine(lineAmount);
+            Console.WriteLine(lineSum);
         }
     }
 }
