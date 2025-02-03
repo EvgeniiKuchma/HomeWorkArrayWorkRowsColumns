5964970659fdbd9144a6552ecc120dc88e004083
Евгений Кучма <desferto87@gmail.com>
2025-02-03 18:50:02 +0500

    lv

--- Program.cs
+++ Program.cs
@@ -6,34 +6,47 @@ namespace HomeWorkArrayWorkRowsColumns
     {
         static void Main(string[] args)
         {
-            int firstColumnMultiplication  = 1;
-            int secondLineAmount = 0;
+            int columnNumber = 0;
+            int rowNumber = 1;
+            int columnMultiplication = 1;
+            int lineAmount = 0;
 
-            int[,] array = new int[3, 5] {
+            int[,] numberesMatrixArray = new int[3, 5] {
                 { 3, 5, 6, 7, 8 },
                 { 3, 3, 3, 3, 3 },
                 { 3, 5, 6, 7, 8 }
             };
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
                 {
-                    Console.Write(array[i, j] + "\t");
-                    if (j == 0)
-                    {
-                       firstColumnMultiplication *= array[i, j];
-                    }
-                    if (i == 1)
-                    {
-                        secondLineAmount += array[i, j];
-                    }
+                    Console.Write(numberesMatrixArray[i, j] + "\t");
                 }
                 Console.WriteLine("\n");
             }
-
-            Console.WriteLine(firstColumnMultiplication);
-            Console.WriteLine(secondLineAmount);
+            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
+                {
+                    if (j == columnNumber)
+                    {
+                        columnMultiplication *= numberesMatrixArray[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < numberesMatrixArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numberesMatrixArray.GetLength(1); j++)
+                {
+                    if (i == rowNumber)
+                    {
+                        lineAmount += numberesMatrixArray[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(columnMultiplication);
+            Console.WriteLine(lineAmount);
         }
     }
 }
